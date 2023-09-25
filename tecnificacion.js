e490e03e45cb59fd61c9897fac28aa7eaad32a1e
Sergio2124 <120379391+Sergio2124@users.noreply.github.com>
2023-09-25 22:40:31 +0200

    i

--- tecnificacion.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var btnMostrar = document.getElementById("btnMostrar");
-    var divOculto = document.getElementById("divoculto");
-
-    btnMostrar.addEventListener("click", function() {
-        divOculto.style.display = "flex";
-    });
-});
